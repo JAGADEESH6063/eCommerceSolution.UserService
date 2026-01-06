ba3a6d93dd7dc436785fc2b257fd2ab3aa921349
Jagadeesh, Neeruganti <Neeruganti.Jagadeesh@Solera.com>
2026-01-06 22:33:43 +0530

    Swagger Implementation

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -3,6 +3,7 @@ using eCommerce.Core;
 using eCommerce.Core.Mappers;
 using eCommerce.InfraStructure;
 using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,13 +27,36 @@ builder.Services.AddAutoMapper(typeof(
 //Adding Fluent validation for Model
 builder.Services.AddFluentValidationAutoValidation();
 
+//Add api explorer services
+builder.Services.AddEndpointsApiExplorer();
+
+//Add Swagger Generation services to generate swagger specification
+builder.Services.AddSwaggerGen();
+
+
+//Add cors
+
+builder.Services.AddCors(
+    options => {
+        options.AddDefaultPolicy(builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+    }
+    );
 //Build app
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 
+//Routing
 app.UseRouting();
+app.UseSwagger();//Adds endpoints to the swagger.json
+app.UseSwaggerUI(); // UI for the swagger page
 
+app.UseCors();// Used for the others apps to work like angular
 //Auth
 app.UseAuthentication();
 app.UseAuthorization();
